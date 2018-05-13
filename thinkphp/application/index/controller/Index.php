7b5aa06ae8a6116089360aad33c79d87e2db342e
dongdongchen <dongdongchen@hk01.com>
2018-05-13 17:38:57 +0800

    案例demo

--- thinkphp/application/index/controller/Index.php
+++ thinkphp/application/index/controller/Index.php
@@ -1,19 +1,21 @@
 <?php
+
 namespace app\index\controller;
 class Index
 {
     public function index()
     {
-        return  '';
+        echo 'asfdsas';
     }
 
-    public function singwa() {
-        echo time();
+    public function singwa()
+    {
+        echo 'sdfsaf' . time();
     }
 
     public function hello($name = 'ThinkPHP5')
     {
-        echo 'hessdggsg' . $name.time();
+        echo 'hessdggsg' . $name . time();
     }
 
 }
--- thinkphp/server/http_server.php
+++ thinkphp/server/http_server.php
@@ -9,14 +9,58 @@ $http = new swoole_http_server("0.0.0.0", 9501);
 
 //静态资源访问设置，如果前端访问的是静态资源则不会走下面的onRequest事件
 $http->set([
+    'worker_num' => 5,
     'enable_static_handler' => true,
     'document_root' => "/dnmp/www/site1/thinkphp/public/static",
     //设置静态文件的根目录  http://47.106.133.191:9501/index.html
 ]);
 
-$http->on('request', function ($request, $response) {
-    $response->cookie('chendongdong', '最帅！', 120);
-    $response->end("<h1>Hello Swoole. #" . rand(1000, 9999) . "</h1>" . json_encode($request->get));
+$http->on('WorkerStart', function (swoole_server $server, $worker_id) {
+    // 定义应用目录
+    define('APP_PATH', __DIR__ . '/../application/');
+    // 加载基础文件
+    require __DIR__ . '/../thinkphp/base.php';
+});
+
+$http->on('request', function ($request, $response) use ($http) {
+    $_SERVER = [];
+    if (isset($request->server)) {
+        foreach ($request->server as $k => $v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    if (isset($request->header)) {
+        foreach ($request->header as $k => $v) {
+            $_SERVER[strtoupper($k)] = $v;
+        }
+    }
+    $_GET = [];
+    if (isset($request->get)) {
+        foreach ($request->get as $k => $v) {
+            $_GET[$k] = $v;
+        }
+    }
+    $_POST = [];
+    if (isset($request->post)) {
+        foreach ($request->post as $k => $v) {
+            $_POST[$k] = $v;
+        }
+    }
+
+    ob_start();
+    // 执行应用并响应
+    try {
+        \think\Container::get('app', [defined('APP_PATH') ? APP_PATH : ''])
+            ->run()
+            ->send();
+    } catch (\Exception $e) {
+        //todo
+    }
+//    echo "-action-" . request()->action() . PHP_EOL;
+    $res = ob_get_contents();
+    ob_end_clean();
+    $response->end($res);
+//    $http->close();
 });
 
 $http->start();
